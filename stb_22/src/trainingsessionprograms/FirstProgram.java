ba1b0737196899437b3d5fd9c1f02c248eb0fb78
Arunodaya1234 <113821001+Arunodaya1234@users.noreply.github.com>
2022-09-18 12:58:27 +0530

    First Commit

---
+++ stb_22/src/trainingsessionprograms/FirstProgram.java
@@ -0,0 +1,10 @@
+package trainingsessionprograms;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This will print on the console");
+	}
+
+}
